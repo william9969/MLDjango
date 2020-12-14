5a81c25c3601fcd256033f5e9610a8ebdf8d7f58
william9969 <wsinchi69@gmail.com>
2020-12-14 13:36:12 -0500

    Modelo Terminado

--- apiSNN/Logica/modeloSNN.py
+++ apiSNN/Logica/modeloSNN.py
@@ -5,6 +5,8 @@ import numpy as np
 from sklearn.pipeline import Pipeline
 from tensorflow.python.keras.models import load_model, model_from_json
 from keras import backend as K
+from skimage.transform import resize   
+import matplotlib.pyplot as plt
 from apiSNN import models
 from keras.preprocessing import image
 import os
@@ -19,7 +21,7 @@ from django.conf import settings
 class modeloSNN():
     """Clase modelo SNN"""
     Selectedmodel = Sequential()
-    CLASS_NAMES=['americano', 'basket', 'beisball', 'boxeo', 'ciclismo', 'f1', 'futbol', 'golf', 'natacion', 'tennis']
+    CLASS_NAMES=['ciclismo', 'basket', 'tenis', 'futbol', 'americano', 'golf', 'beisball', 'boxeo', 'natacion', 'f1']
     IMAGE_SIZE = (21, 28)
 
     def cargarRNN(archivoModelo,archivoPesos):
@@ -39,39 +41,55 @@ class modeloSNN():
         self.Selectedmodel = self.cargarRNN(archivoModelo, archivoPesos) 
         print(self.Selectedmodel)
         print(self.Selectedmodel.summary())
-
-        img = Image.open(settings.BASE_DIR + path).convert('RGB')
-        #img = img.resize(self.IMAGE_SIZE)
-        img.save(settings.BASE_DIR + path, "JPEG", optimize=True)
-
-
-        img = self.preprocesamiento(self, img=img)
-        maxElement, certeza,predicion = self.predict(self, img)
+        img = self.preprocesamiento(self, path=path)
+        certeza,predicion = self.predict(self, img)
+        print('La prediccion es ',predicion )
         #dbReg = models.Image(image=path, label=prediction_result, probability=maxElement)
         #dbReg.save()
         return (certeza,predicion)
 
     def predict(self, imgT):
-        print('---------------------------------------------------------------------------')
-        print(imgT)
-        predicted_classes = self.Selectedmodel.predict(imgT)[0]
-        print('Predictions: ', predicted_classes)
-        maxElement =numpy.amax(predicted_classes)
-        certeza=str(round(maxElement*100,4))+'%'
+        #print('---------------------------------------------------------------------------')
+       # print(imgT)
+        predicted_classes = self.Selectedmodel.predict(imgT)
+        print("Predicciones")
+        #predicted_classes = sport_model.predict(test_X)
+        #resultados = sport_model.predict(predicted_classes)
+        print(predicted_classes)
+        maxElement = numpy.amax(predicted_classes)
+        certeza=str(round(maxElement*100,4))
         print('Certeza: ', str(round(maxElement*100,4))+'%')
-        result=numpy.where(predicted_classes == numpy.amax(predicted_classes)) 
+        result = numpy.where(predicted_classes == numpy.amax(predicted_classes))
         print('Max :',maxElement )
         print('Lista de indices de maximo elemento : ' , result[0][0])
         index_sample_label = result[0][0] 
-        predicion = self.CLASS_NAMES[index_sample_label]
-        return (maxElement, certeza,predicion)
+
+        print(self.CLASS_NAMES)
+        for i, img_tagged in enumerate(predicted_classes):
+            predicion=self.CLASS_NAMES[img_tagged.tolist().index(max(img_tagged))]
+            #print("Cantidad etiquetas creadas: ",len(labels))
+            print('Etiqueta de Prediccion :',self.CLASS_NAMES[img_tagged.tolist().index(max(img_tagged))])
+
+
+        return (certeza,predicion)
         
-    def preprocesamiento(self, img):
-        
-        X = np.array(img, dtype=np.uint8)
-        #test_X = X.reshape(1, self.IMAGE_SIZE[0], self.IMAGE_SIZE[1], 3)
+    def preprocesamiento(self, path):
+        images=[]
+
+        for filepath in path:
+            image = plt.imread(settings.BASE_DIR + path,0)
+            image_resized = resize(image, (21, 28),anti_aliasing=True,clip=False,preserve_range=True)
+            images.append(image_resized)
+
+        X = np.array(images, dtype=np.uint8) #convierto de lista a numpy
         test_X = X.astype('float32')
-        test_X = test_X / 255. 
-        print(X)
+        test_X = test_X / 255.
+        #img = img.resize(self.IMAGE_SIZE , clip=False)
+        #imgResizec = resize(img, self.IMAGE_SIZE,anti_aliasing=True,clip=False,preserve_range=True)
+        #X = np.array(img, dtype=np.uint8)
+        #test_X = X.reshape(1, self.IMAGE_SIZE[0], self.IMAGE_SIZE[1], 3)
+        #test_X = X.astype('float32')
+        #test_X = X / 255. 
+        #print(X)
         return test_X
 
