df86e42823db7366ac6e36e42dc8f2370694ed46
jessica-maribel <jessicaguncay45@gmail.com>
2020-12-14 02:50:46 -0500

    Eliminado

--- apiSNN/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from apiSNN.models import Image
-
-admin.site.register(Image)
--- apiSNN/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from apiSNN.models import UploadImage
-
-class ImageForm(forms.ModelForm):
-    
-    class Meta:
-        model = UploadImage
-        fields = ['image']
